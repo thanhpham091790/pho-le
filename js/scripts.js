3de6a226879c7ccabe2a236173ad0bbac353919d
Thanh Pham <thanhpham091790@gmail.com>
2021-01-10 21:27:45 -0700

    Add data to json file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
+// Load menu category when click 
+function showActive(element){
+    document.getElementById("menu-active").innerHTML = element.innerHTML;
+}
 
+// Load content of website via AJAX
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -21,15 +26,20 @@ xmlhttp.onreadystatechange = function () {
         document.getElementById("about-info").innerHTML = about_info;
 
         // Load menu category from json file
-        var menu_nav = "";
-        for (i = 0; i < resta.categories.length; i++) {
-            menu_nav += '<a class="dropdown-item text-dark m-0 py-3 border-bottom" data-toggle="tab" \
-            href = "#'+ resta.categories[i].cid +'">'+ resta.categories[i].cename +'<span class="d-block">'+ resta.categories[i].cvname +'</span></a>';
+        var cats = resta.categories;
+        var menu_nav = "", is_first;
+        for (i = 0; i < cats.length; i++) {
+            if (cats[i].cposition == "0") {
+                is_first = 'id="first-cat"';
+            } else {
+                is_first = "";
+            }
+            menu_nav += '<a onclick="showActive(this)" '+ is_first +' class="dropdown-item text-dark m-0 py-3 border-bottom" data-toggle="tab" \
+            href = "#'+ cats[i].cid + '">' + cats[i].cename + '<span class="d-block">' + cats[i].cvname + '</span></a>';
         }
         document.getElementById("menu-nav").innerHTML = menu_nav;
 
         // Load menu items from json file
-        var cats = resta.categories;
         var menu = "", active;
         for (i = 0; i < cats.length; i++) {
             if (cats[i].cposition == "0") {
@@ -38,75 +48,82 @@ xmlhttp.onreadystatechange = function () {
                 active = "";
             }
             menu += '\
-        <div id="'+ cats[i].cid + '" class="container-fluid tab-pane ' + active + '">\
-            <div class="row"> \
-                <div class="col-12"> \
-                    <h3 class="c-name text-success">'+ cats[i].cename + '</h3> \
-                </div> \
-                <div class="col-12"> \
-                    <img class="c-img img-fluid" src="'+ cats[i].cimage + '" \
-                        alt="'+ cats[i].cname + '"> \
-                </div> \
-                <div class="col-12"> \
-                    <p class="c-desc text-success">'+ cats[i].cdesc + '</p> \
-                </div> \
-            </div>';
-            var j;
-            var pros = cats[i].cproducts;
-            for (j = 0; j < pros.length; j++) {
-                menu += '\
-            <div class="row"> \
-                <div class="col-8"> \
-                    <p class="p-name">A1. Goi Cuon - Steamed Spring Roll (2pcs) '+ pros[j].pid + '. ' + pros[j].pvname + ' - ' + pros[j].pename + ' (' + pros[j].ppieces + ')' + ' \
-                        <span class="p-img"><i class="fas fa-camera text-success" data-toggle="modal" \
-                                data-target="#'+ pros[j].pid + '"></i></span> \
-                    <div class="modal fade" id="'+ pros[j].pid + '"> \
-                        <div class="modal-dialog modal-dialog-centered"> \
-                            <div class="modal-content"> \
-                                <img class="img-fluid" src="'+ pros[j].pimage + '" alt="' + pros[j].pename + '"> \
+            <div id="'+ cats[i].cid + '" class="container-fluid  px-0 tab-pane ' + active + '">\
+                <div class="row"> \
+                    <div class="col-12 text-center"> \
+                        <h3 class="c-name text-success">'+ cats[i].cename +'</h3> \
+                    </div> \
+                    <div class="col-12"> \
+                        <img class="c-img img-fluid w-100" src="'+ cats[i].cimage + '" \
+                            alt="'+ cats[i].cname + '"> \
+                    </div> \
+                    <div class="col-12"> \
+                        <p class="c-desc text-success">'+ cats[i].cdesc + '</p> \
+                    </div> \
+                </div>';
+                var j;
+                var pros = cats[i].cproducts;
+                for (j = 0; j < pros.length; j++) {
+                    var pvname="", ppieces="",pimage="";
+                    if(pros[j].pvname != ""){pvname = pros[j].pvname + ' - ';}
+                    if(pros[j].ppieces != ""){ppieces = ' (' + pros[j].ppieces + ')';}
+                    if(pros[j].pimage != ""){pimage = '<i class="fas fa-camera text-success" data-toggle="modal"';}
+                    menu += '\
+                    <div class="row"> \
+                        <div class="col-8"> \
+                            <p class="p-name">'+ pros[j].pid + '. ' + pvname + pros[j].pename + ppieces + ' \
+                                <span class="p-img"> '+ pimage +'\
+                                        data-target="#'+ pros[j].pid + '"></i></span> \
+                            <div class="modal fade" id="'+ pros[j].pid + '"> \
+                                <div class="modal-dialog modal-dialog-centered"> \
+                                    <div class="modal-content"> \
+                                        <img class="img-fluid" src="'+ pros[j].pimage + '" alt="' + pros[j].pename + '"> \
+                                    </div> \
+                                </div> \
+                            </div> \
+                            </p> \
+                            <p class="p-desc">'+ pros[j].pdesc + '</p> \
+                        </div> \
+                        <div class="col-4"> \
+                            <div class="p-price">';
+                        var z;
+                        var opts = pros[j].prices;
+                        if (opts.length == 1) {
+                            menu += '<p>' + opts[0].price + '</p>';
+                        } else {
+                            for (z = 0; z < opts.length; z++) {
+                                menu += '<p>' + opts[z].price + ' (' + opts[z].size + ')</p>';
+                            }
+                        }
+                        menu += '\
                             </div> \
                         </div> \
-                    </div> \
-                    </p> \
-                    <p class="p-desc">'+ pros[j].pdesc + '</p> \
-                </div> \
-                <div class="col-4"> \
-                    <div class="p-price">';
-                var z;
-                var opts = pros[j].prices;
-                if (opts.length == 1) {
-                    menu += '<p>' + opts[0].price + '</p>';
-                } else {
-                    for (z = 0; z < opts.length; z++) {
-                        menu += '<p>' + opts[z].price + ' (' + opts[z].size + ')</p>';
-                    }
+                    </div>';
                 }
-                menu += '\
-                    </div> \
-                </div> \
-            </div>';
-            }
             menu += '\
-        </div>';
+            </div>';
         }
         document.getElementById("menu-items").innerHTML = menu;
+
+        // Auto click to active the first category
+        document.getElementById("first-cat").click();
 
         // Load address and phone from json file
         var address_info = "";
         address_info = '<a class="text-dark text-decoration-none" \
-        href="'+ resta.address[0] +'">'+ resta.address[1] +'</a>';
+        href="'+ resta.address[0] + '">' + resta.address[1] + '</a>';
         document.getElementById("address-info").innerHTML = address_info;
 
         var phone_info = "";
         phone_info = '<a class="text-dark text-decoration-none" \
-        href="tel:'+ resta.phone[0] +'">'+ resta.phone[1] +'</a>';
+        href="tel:'+ resta.phone[0] + '">' + resta.phone[1] + '</a>';
         document.getElementById("phone-info").innerHTML = phone_info;
     }
 };
 xmlhttp.open("GET", "menu.json", true);
 xmlhttp.send();
 
-
+// Load Google Map section
 function myMap() {
     var mapOptions = {
         center: new google.maps.LatLng(39.66743209145535, -104.86360942372228),
